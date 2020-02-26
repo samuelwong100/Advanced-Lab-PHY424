f1ab10df2357e62cea1bfa369241f641a238c433
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2020-02-25 20:35:48 -0500

    wrote and tested load and plot

--- COMP/compton_analysis.py
+++ COMP/compton_analysis.py
@@ -8,15 +8,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from my_odr_fit import my_odr_fit
 
-def load_and_plot(file,title):
+def load(file):
     data = np.loadtxt(file)
     channel = data[:,0]
     counts = data[:,1]
+    return channel, counts
+
+def load_adjusted(file):
+    channel, counts = load(file)
+    counts = counts - noise
+    return counts
+
+def plot(channel,counts,title):
     plt.figure()
     plt.title(title)
     plt.scatter(channel,counts)
+    plt.savefig(title+".png")
     plt.show()
+    
+def load_and_plot(file,title):
+    channel, counts = load(file)
+    plot(channel,counts,title)
     return channel, counts
 
-load_and_plot("data/aluminum#1.txt","aluminum#1")
-load_and_plot("data/noise.txt","noise")
+def load_adjusted_and_plot(file,title):
+    counts = load_adjusted(file)
+    plot(channel,counts,title)
+    return counts
+
+#load and plot the noise counts
+channel, noise = load_and_plot("data/noise.txt","noise")
+#load and plot counts for different absorbers with noise subtracted
+alum1 = load_adjusted_and_plot("data/aluminum#1.txt","aluminum#1")
+                               
+
